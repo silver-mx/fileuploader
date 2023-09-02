ba95d4647893cba939d9535626e54ee631f2b019
Diego Núñez Silva <diego.nunez.mx@gmail.com>
2023-09-02 14:04:09 +0200

    Initial commit

---
+++ src/main/java/dns/fileuploader/FileuploaderApplication.java
@@ -0,0 +1,13 @@
+package dns.fileuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileuploaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dns/fileuploader/fileuploader/FileuploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package dns.fileuploader.fileuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileuploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
